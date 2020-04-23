59cc2ae5f150487b3871519d16419e337afb23c6
Jia Zhang <zhangjia_gl@163.com>
2020-04-23 19:09:23 +0800

    Update demo_GRRO_LS.m

--- demo_GRRO_LS.m
+++ demo_GRRO_LS.m
@@ -1,4 +1,7 @@
-% This is an example file on how the GRRO_LS program could be used.
+% This is an example file on how the GRRO_LS program [1] could be used.
+
+% J. Zhang, Y. Lin, M. Jiang, S. Li, Y. Tang, K. C. Tan:
+% Multi-label feature selection via global relevance and redundancy optimization. In IJCAI, Yokohama, Japan, 2020.
 
 % Please feel free to contact me (zhangjia_gl@163.com), if you have any problem about this program.
 
